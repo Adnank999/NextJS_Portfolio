340f771e5d2473e0c8fa70cd437052d1374cf4f9
Adnank999 <adnanhossain4ever@gmail.com>
2023-09-26 10:04:41 +0600

    third Commit

--- app/components/Space3d.js
+++ app/components/Space3d.js
@@ -3,17 +3,18 @@ import { Suspense } from "react";
 import { Canvas, useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls, useAnimations } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import SpaceEX from "./SpaceEx";
 
 const Model = () => {
   const gltf = useLoader(GLTFLoader, "/space/scene.gltf");
 
-  const { actions } = useAnimations(gltf.animations, gltf.scene);
+  // const { actions } = useAnimations(gltf.animations, gltf.scene);
 
-  actions.Animation.play();
+  // actions.Animation.play();
 
   return (
     <mesh>
-      <meshStandardMaterial />
+      
       <primitive object={gltf.scene} scale={0.8} />
     </mesh>
   );
@@ -23,15 +24,11 @@ const Space3d = () => {
   return (
     <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
       <ambientLight intensity={1.5} />
-      <spotLight
-        intensity={1}
-        angle={0.1}
-        penumbra={1}
-        position={[10, 15, 10]}
-        castShadow
-      />
+      <pointLight position={[10, 10, 10]} />
       <Suspense fallback={null}>
         <Model />
+
+        
         <Environment preset={"night"} />
       </Suspense>
 
